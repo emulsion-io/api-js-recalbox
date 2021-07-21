554bd4afaca17ccacec2b403f8c862c4db746bc1
Fabrice <aigleblanc@gmail.com>
2021-07-21 19:08:12 +0200

    first commit
    WIP

---
+++ endpoint.md
@@ -0,0 +1,58 @@
+# Recalbox Endpoint POST
+
+## Configuration
+
+```json
+{
+   "system.language": "fr_FR",
+   "system.kblayout": "us",
+   "system.timezone": "Europe/Paris",
+   "system.hostname": "RECALBOX",
+   "system.es.menu": "",
+   "system.emulators.specialkeys": "default",
+   "system.api.enabled": "0",
+   "wifi.enabled": "0",
+   "wifi.ssid": "Viky",
+   "wifi.key": "",
+   "kodi.enabled": "0",
+   "kodi.atstartup": "0",
+   "kodi.xbutton": "0",
+   "emulationstation.selectedsystem": "favorites",
+   "emulationstation.bootongamelist": "0",
+   "emulationstation.hidesystemview": "0",
+   "emulationstation.gamelistonly": "1",
+   "updates.enabled": "1",
+   "updates.type": "stable"
+}
+```
+
+## Controleurs
+
+```json
+{
+   "controllers.db9.enabled": "0",
+   "controllers.db9.args": "map=1",
+   "controllers.gamecon.enabled": "0",
+   "controllers.gamecon.args": "map=1",
+   "controllers.gpio.enabled": "0",
+   "controllers.gpio.args": "map=1,2",
+   "controllers.ps3.enabled": "1",
+   "controllers.ps3.driver": "bluez"
+}
+```
+
+## Systemes
+
+```json
+{
+   "global.ratio": "auto",
+   "global.shaderset": "scanlines",
+   "global.smooth": "1",
+   "global.rewind": "0",
+   "global.autosave": "0",
+   "global.integerscale": "0",
+   "global.retroachievements.hardcore": "0",
+   "global.retroachievements.username": "",
+   "global.retroachievements.password": ""
+}
+```
---
+++ recal.js
@@ -0,0 +1,280 @@
+const axios = require('axios');
+
+const url = "http://192.168.1.25"
+
+const user = {
+	login : "fabrice",
+	password : "test"
+}
+
+class Recal {
+
+	cookie = false;
+	recal;
+	baseAxio =	{
+		baseURL: url,
+		timeout: 1000,
+	}
+
+	constructor() {
+				
+		this.recal = axios.create(this.baseAxio);
+		this.actionNeedAuth()
+
+	}
+
+	getParser(option) {
+		return this.recal.get('/get?option=' + option)
+			.then(function (response) {
+				return response.data.data;
+			})
+	}
+
+	grepParser(option) {
+		return this.recal.get('/grep?keys=' + option.join("|"))
+			.then(function (response) {
+				return response.data.data;
+			})
+	}
+
+	postSave(option) {
+		let payload = option;
+		return this.recal.post('/save', payload)
+			.then(function (response) {
+				
+				return response.data.data;
+			})
+	}
+
+	postAction(option, payload = {}) {
+		return this.recal.post('/post?action=' + option, payload)
+			.then(function (response) {
+				return response;
+			})
+			.catch(function (error) {
+			  console.log(error);
+			})
+	}
+
+	/**
+	 * 
+	 */
+	getTemp() {
+		var option = "temperature"
+
+		this.getParser(option).then((returnval) => {
+			console.log(returnval.temperature.current);
+		});
+	}
+
+	/**
+	 * 
+	 */
+	getCpu() {
+		var option = "cpus"
+
+		this.getParser(option).then((returnval) => {
+			console.log(returnval.cpus);
+		});
+	}
+
+	/**
+	 * 
+	 */
+	getRam() {
+		var option = "ram"
+
+		this.getParser(option).then((returnval) => {
+			console.log(returnval.ram);
+		});
+	}
+
+	/**
+	 * 
+	 */
+	getDisk() {
+		var option = "disks"
+
+		this.getParser(option).then((returnval) => {
+			console.log(returnval.disks);
+		});
+	}
+
+	/**
+	 * 
+	 */
+	 getVolume() {
+		var option = ['audio.device','audio.volume','audio.bgmusic']
+
+		this.grepParser(option).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	saveVolume(volume) {
+		var option = {
+			'audio.volume': volume
+		}
+
+		this.postSave(option).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	saveKodi() {
+		var option = {
+			"kodi.enabled": "0",
+			"kodi.atstartup": "0",
+			"kodi.xbutton": "0"
+		}
+
+		this.postSave(option).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	saveWifi(enabled, ssid, pass) {
+
+		let passw = pass.replace('#', '\#');
+
+		var option = {
+			"wifi.enabled": enabled,
+			"wifi.ssid": ssid,
+			"wifi.pass": passw
+		}
+
+		this.postSave(option).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionTakeScreen() {
+		var action = "takeScreenshot"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionDelScreen(file) {
+		var action = "deleteScreenshot"
+
+		var option = {
+			"file": file
+		}
+
+		this.postAction(action, option).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	getScreen() {
+		var action = "screenshotsList"
+
+		this.getParser(action).then((returnval) => {
+			console.log(returnval.screenshotsList);
+		});
+	}
+
+	/**
+	 * @return ok or ko
+	 */
+	getStatusES() {
+		var option = "ESStatus"
+
+		this.getParser(option).then((returnval) => {
+			console.log(returnval.ESStatus);
+		});
+	}
+
+	actionRebootES(action) {
+		var action = "reboot-es"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionShutdownES(action) {
+		var action = "shutdown-es"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionStartES(action) {
+		var action = "start-es"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionRebootOS(action) {
+		var action = "reboot-os"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionShutdownOS(action) {
+		var action = "shutdown-os"
+
+		this.postAction(action).then((returnval) => {
+			console.log(returnval);
+		});
+	}
+
+	actionLogin() {
+		var action = "login"
+
+		let option = {
+			"login": user.login,
+			"password": user.password
+		};
+
+		this.postAction(action, option).then((returnval) => {
+
+			let cookie1s = returnval.headers['set-cookie'][0].split(';')
+			let cookie2s = returnval.headers['set-cookie'][1].split(';')
+			
+			this.cookie = cookie1s[0] + '; ' + cookie2s[0]
+
+			this.baseAxio.headers = {
+				'Cookie': this.cookie
+			}
+			
+			console.log('login ok');
+
+			this.recal = axios.create(this.baseAxio);
+		});
+	}
+
+	actionNeedAuth() {
+		var option = "needAuth"
+
+		this.getParser(option).then((returnval) => {
+
+			if(returnval.needAuth === true) {
+				this.actionLogin();
+				console.log('need auth', returnval.needAuth);
+			}
+
+		});
+	}
+
+	getCookie() {
+		console.log('retour de mon cookies apres login', this.cookie);
+	}
+
+}
+
+var api = new Recal();
+
+//api.getStatusES();
+
+api.getCookie()
+
+//api.actionTakeScreen();
